a4c5af57a3ca8de16408d403f9238a950368a7ae
Zeus-2321 <109408148+Zeus-2321@users.noreply.github.com>
2023-01-02 21:54:35 +0530

    Add files via upload

---
+++ src/AppClass.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+export default class AppClass extends Component{
+
+  render(){
+    return(
+      <div className='App'>
+        <h1>Kalvium Gallery</h1>
+        <div className='Gallery'>
+          {this.props.imageData.map((e) => {
+           return (<img src={e.img} alt='img(i)' />)
+        })}
+        </div>
+      </div>
+    );
+  }
+}
